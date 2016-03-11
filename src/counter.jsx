5e9c0585ef7c24e7ed1c26bd95997fa9e71cc60b
Yohei Kitamura <la_kitamura@worksap.co.jp>
2016-03-11 22:59:28 +0900

    try create react-bootstrap navibar

--- src/counter.jsx
+++ src/counter.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import { Navbar, Nav,NavDropdown,NavItem,MenuItem } from 'react-bootstrap';
 
 export default class Counter extends Component {
     constructor() {
@@ -14,12 +15,34 @@ export default class Counter extends Component {
         });
     }
 
+
     render() {
+      var style = {
+        logo: {
+          background: "url('images/SUSUMATE-logo.png')",
+          height: "45",
+          width: "136"
+        }
+      }
         return (
-            <div>
-                Count: {this.state.count}
-                <button onClick={this.handleClick.bind(this)}>increment</button>
-            </div>
+          <Navbar>
+  <Navbar.Header>
+    <Navbar.Brand>
+      <a style={style.logo} href="#"></a>
+    </Navbar.Brand>
+  </Navbar.Header>
+  <Nav>
+    <NavItem eventKey={1} href="#">Link</NavItem>
+    <NavItem eventKey={2} href="#">Link</NavItem>
+    <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+      <MenuItem eventKey={3.1}>Action</MenuItem>
+      <MenuItem eventKey={3.2}>Another action</MenuItem>
+      <MenuItem eventKey={3.3}>Something else here</MenuItem>
+      <MenuItem divider />
+      <MenuItem eventKey={3.3}>Separated link</MenuItem>
+    </NavDropdown>
+  </Nav>
+</Navbar>
         );
     }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,34 @@
 const path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
-    entry: {
-        bundle: './src/app.jsx'
-    },
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: '[name].js'
-    },
-    module: {
-        loaders: [
-            {
-                loader: 'babel',
-                exclude: /node_modules/,
-                test: /\.js[x]?$/,
-                query: {
-                    cacheDirectory: true,
-                    presets: ['react', 'es2015']
-                }
-            }
-        ]
-    }
+  entry: {
+    bundle: './src/app.jsx'
+  },
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: '[name].js'
+  },
+  devServer: {
+    contentBase: './public', //Relative directory for base of server
+    hot: true, //Live-reload
+    inline: true,
+    port: 3000, //Port Number
+    host: 'localhost' //Change to '0.0.0.0' for external facing server
+  },
+  plugins: [
+    // hotモードに必要なプラグイン
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  module: {
+    loaders: [{
+      loader: 'babel',
+      exclude: /node_modules/,
+      test: /\.js[x]?$/,
+      query: {
+        cacheDirectory: true,
+        presets: ['react', 'es2015']
+      }
+    }]
+  }
 };
