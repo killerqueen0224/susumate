d51de2e282b029c29ad56cdb4479cc2606430d71
Yohei Kitamura <la_kitamura@worksap.co.jp>
2016-04-08 23:45:47 +0900

    change css file

--- src/body.jsx
+++ src/body.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import { Grid, Col, Row } from 'react-bootstrap';
+<<<<<<< HEAD
 import styles from '../public/css/main.css';
 
+=======
+
+// require('../public/css/main.css');
+
+>>>>>>> 0b0f5bd8e2b0ffaf84f07c18a917c0c92af9ccba
 export default class Body extends React.Component {
 
   render() {
     var style = {
       body: {
+        fontFamily:"01フロップデザイン",
         background: "red",
         paddingTop: "70px"
       },
